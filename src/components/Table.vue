d0dc48131d359d719b7135f881df81bf0b2f3f0a
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-09-11 01:37:04 +0900

    mini.css 테이블 코드 예제 붙여 넣음

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,31 @@
 <template>
-    
+    <table>
+        <caption>People</caption>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Alias</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td data-label="Name">Chad</td>
+            <td data-label="Surname">Wilberts</td>
+            <td data-label="Alias">MrOne</td>
+        </tr>
+        <tr>
+            <td data-label="Name">Adam</td>
+            <td data-label="Surname">Smith</td>
+            <td data-label="Alias">TheSmith</td>
+        </tr>
+        <tr>
+            <td data-label="Name">Sophia</td>
+            <td data-label="Surname">Canderson</td>
+            <td data-label="Alias">Candee</td>
+        </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
