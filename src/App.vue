ca262898b237ac8a74097092f41fc59710247377
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-09-11 19:44:31 +0900

    인덴트를 4space 로 변경함

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,46 @@
 <template>
-  <div id="app">
-    <Table :columes="columes" :data="statusList"/>
-  </div>
+    <div id="app">
+        <Table :columes="columes" :data="statusList"/>
+    </div>
 </template>
 
 <script>
 import Table from './components/Table'
 
 export default {
-  name: 'app',
-  components: {
-    Table
-  },
-  data() {
-    return {
-        columes: {
-            name : "Name",
-            url : "URL",
-            method : "Method",
-            status : "Status"
-        },
-        statusList : [
-            {
-                name : "사용자 등록",
-                url : "/user",
-                method : "POST",
-                status : "SUCCESS"
-            }
-        ]
+    name: 'app',
+    components: {
+        Table
+    },
+    data() {
+
+        return {
+            columes: {
+                name : "Name",
+                url : "URL",
+                method : "Method",
+                status : "Status"
+            },
+            statusList : [
+                {
+                    name : "사용자 등록",
+                    url : "/user",
+                    method : "POST",
+                    status : "SUCCESS"
+                }
+            ]
+        }
     }
-  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
